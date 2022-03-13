809fa24db12e07f4b1ac9776d8a0ab1f1aee2a27
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-13 19:00:49 +0300

    adding the skeleton

---
+++ game_of_greed/banker.py
@@ -0,0 +1,14 @@
+class Banker():
+
+    def __init__(self):
+        pass
+
+    def shelf(self, int):
+        pass
+
+    def bank(self):
+        pass
+        
+    def clear_shelf(self):
+        self.shelved = 0
+        return self.shelved
---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from random import randint
+
+class GameLogic:
+    @staticmethod
+    def roll_dice(num):
+        pass 
+
+  
+    @staticmethod
+    def calculate_score(dice):
+        pass
+
+
+
+
+
+
+
+

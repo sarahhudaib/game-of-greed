614281420f77644e63ba922410b2808c59494acd
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-03-14 10:20:19 +0200

    Anas

--- game_of_greed/banker.py
+++ game_of_greed/banker.py
@@ -1,10 +1,12 @@
 class Banker():
 
-    def __init__(self):
-        pass
+    def __init__(self, balance=0, shelved=0):
+        self.balance = balance
+        self.shelved = shelved
 
-    def shelf(self, int):
-        pass
+    def shelf(self, points = 0):
+        self.shelved += points
+        return self.shelved
 
     def bank(self):
         pass

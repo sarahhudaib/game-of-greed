72041307503eeb7d3fbc994d097dcdd66d662911
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-16 17:04:32 +0300

    add 100% of the solution

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -12,23 +12,29 @@ class Game:
         
 
     def quit(score):
-        print(f"Total score is {score} points")
-        print(f"Thanks for playing. You earned {score} points")
+        if score ==0:
+            print(f"Thanks for playing. You earned {score} points")
+        else:   
+            print(f"Total score is {score} points")
+            print(f"Thanks for playing. You earned {score} points")
         
         
     def play(self):
-        round=0
-        unbanked=0
-        rolled=0
-        remaining = 6
         print("Welcome to Game of Greed")
         wanna_play = input("Wanna play? ")
         if wanna_play == "n":
             print("OK. Maybe another time")
         elif wanna_play =="y":
-            while True:
-                round=round+1
-                print(f"Starting round {round}")
+            score=0
+            round=1
+            unbanked=0
+            rolled=0
+            remaining = 6
+            play=True
+            print(f"Starting round {round}")
+           
+            while play:
+                
                 print("Rolling 6 dice...")
                 rolled_dice = self.roller(6)
                 nums = []
@@ -37,26 +43,43 @@ class Game:
                 print(','.join(nums))
                 decision = input("Enter dice to keep (no spaces), or (q)uit: ")
                 if decision== "q":
-                    print("Thanks for playing. You earned 0 points")
-                    break
+                    Game.quit(score)
+                    rolled=0
+                    play=False
                 elif decision != 'q' and decision != 'b'and decision != 'r':
                     user_input = [int(i) for i in decision]
                     remaining -= len(user_input)
                     unbanked = GameLogic().calculate_score(tuple(user_input)) 
                     if rolled!=0 :
-                        unbanked +=rolled   
-                        rolled=0
-            
-                    # remaining= 6-len(decision)
+                      unbanked +=rolled   
+                    rolled=0
                     print(f"You have {unbanked} unbanked points and {remaining} dice remaining")
+                    # round=round+1
                     decision2 = input('(r)oll again, (b)ank your points or (q)uit ')
                     if decision2 == 'q':
-                        Game.quit(self.banker.bank)
-                        break
+                        Game.quit(score)
+                        rolled=0
+                        play=False
+                    
                     elif decision2 == 'b':
-                        print(f'You banked {self.banker.bank} points in round {round}')
+                        score+=unbanked
+                        print(f'You banked {unbanked} points in round {round}')
+                        print(f'Total score is {score} points')
+                        round+=1
+                        rolled=0
+                        remaining =6
+                        print(f'Starting round {round}')
+                    
+                    elif decision2 == 'r' and unbanked == 1500:
+                        rolled += unbanked
+                        remaining =6
                     elif decision2 == 'r':
-                        print(f"Total score is {self.banker.bank} points")
+                        rolled += unbanked
+                        if remaining==0:
+                            score+=unbanked
+                            remaining =6
+                            print(f'Starting round {round}')
+                        
                     
                 
             

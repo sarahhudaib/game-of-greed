94b0cf058e9c857b84c82a7c7308ead8853ff284
e97m <majdalawi97@yahoo.com>
2022-03-21 13:23:11 +0300

    add test simulation files

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -9,26 +9,32 @@ class Game:
         self.game_logic=GameLogic()
         # python -m game_of_greed.game
         
-    # @staticmethod
-    # def quit(score):
-    #     print(f"Total score is {score} points")
-    #     print(f"Thanks for playing. You earned {score} points")
+
+    def quit(score):
+        if score == 0:
+            print(f"Thanks for playing. You earned {score} points")
+        else:
+            print(f"Total score is {score} points")
+            print(f"Thanks for playing. You earned {score} points")
+        
         
         
     def play(self):
-        round=0
-        unbanked=0
-        rolled=0
-        remaining = 6
-        score = 0
         print("Welcome to Game of Greed")
         wanna_play = input("Wanna play? ")
         if wanna_play == "n":
             print("OK. Maybe another time")
         elif wanna_play =="y":
-            print(f"Starting round {round+1}")
-            while True:
-                round=round+1
+            score=0
+            round=1
+            unbanked=0
+            rolled=0
+            remaining = 6
+            play=True
+            print(f"Starting round {round}")
+           
+            while play:
+                
                 print("Rolling 6 dice...")
                 rolled_dice = self.roller(6)
                 nums = []
@@ -36,38 +42,43 @@ class Game:
                     nums.append(str(i))
                 print(','.join(nums))
                 decision = input("Enter dice to keep (no spaces), or (q)uit: ")
-                # print('round', round)
-                if decision == "q" and round <= 1:
-                    print(f"Thanks for playing. You earned {score} points")
-                    break
+                if decision== "q":
+                    Game.quit(score)
+                    rolled=0
+                    play=False
                 elif decision != 'q' and decision != 'b'and decision != 'r':
                     user_input = [int(i) for i in decision]
                     remaining -= len(user_input)
                     unbanked = GameLogic().calculate_score(tuple(user_input)) 
                     if rolled!=0 :
-                        unbanked +=rolled
-                        rolled=0
+                      unbanked +=rolled   
+                    rolled=0
                     print(f"You have {unbanked} unbanked points and {remaining} dice remaining")
                     decision2 = input('(r)oll again, (b)ank your points or (q)uit ')
                     if decision2 == 'q':
-                        print(f"Total score is {score} points")
-                        print(f"Thanks for playing. You earned {score} points")
-                        break
+                        Game.quit(score)
+                        rolled=0
+                        play=False
+                    
                     elif decision2 == 'b':
-                        score += unbanked
-                        print(f'You banked {unbanked} points in round {round-1}')
+                        score+=unbanked
+                        print(f'You banked {unbanked} points in round {round}')
                         print(f'Total score is {score} points')
-                        print(f"Starting round {round}")
-
-                    # elif decision2 == 'r':
-                        # continue
+                        round+=1
+                        rolled=0
+                        remaining =6
+                        print(f'Starting round {round}')
+                    
                     elif decision2 == 'r' and unbanked == 1500:
                         rolled += unbanked
                         remaining =6
                     elif decision2 == 'r':
                         rolled += unbanked
-                        remaining =0
-                        print(f"Starting round {round}")
+                        if remaining==0:
+                            score+=unbanked
+                            remaining =6
+                            print(f'Starting round {round}')
+                        
                     
                 
             
